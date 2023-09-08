eefbcbd0df99eb647092e4c4a7f1c484f68d2e96
“yangxinyuee” <2845591106@qq.com>
2023-09-08 08:19:55 +0800

    creat a new branch

---
+++ test.c
@@ -0,0 +1,16 @@
+// copy the test code on 2023.9.8
+
+#include <stdio.h>
+void printout()
+{
+    // edit your code here
+    printf("hello world!\n");
+}
+int main()
+{
+
+    // call the printout function
+    printout();
+
+    ruturn 0;
+}
